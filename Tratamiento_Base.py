f52037b684e0cdc364edf15463fcba3bea68775f
sbaldoncini13 <43922260+sbaldoncini13@users.noreply.github.com>
2024-09-02 01:42:23 -0300

    Create Tratamiento_Base.py

---
+++ Tratamiento_Base.py
@@ -0,0 +1,295 @@
+import chess.pgn
+import pandas as pd
+from collections import Counter
+import re
+
+
+eco_lines = """
+A00              Aperturas Irregulares
+A01              Ataque Larsen
+A02-A03                Apertura Bird
+      A03                Variante Holandesa
+A04-A09                Ataque Indio de Rey
+A10              Apertura Inglesa
+A11-A12                Apertura Reti
+A13-A39                Apertura Inglesa
+      A17                Variante Erizo
+      A20-A29                  Siciliana Invertida
+      A30-A31                  Variante Erizo
+      A32-A39                  Variante Simetrica
+A40              Gambito Englund
+A41-A42                Defensa Moderna
+A43-A44                Defensa Benoni Antigua
+A45              Ataque Trompovsky
+A46              Partida de Peon de Dama
+A47              Defensa India de Dama
+A48-A49                India de Rey, Ataque Torre
+A50              Defensa India de Dama
+A51-A52                Gambito Budapest
+      A52                Linea Principal
+A53-A55                Defensa India Antigua
+A56              Defensa Benoni Checa
+A57-A59                Gambito Benko (Volga)
+      A58                Aceptado
+A60-A79                 Defensa Benoni Moderna
+A80-A99                 Defensa Holandesa
+      A82                 Gambito Staunton
+      A83                 Linea Principal
+      A87-A90                   Leningrado
+B00               Defensa Nimzowitsch; Defensa Owens
+B01               Defensa Escandinava
+B02-B05                 Defensa Alekhine
+      B03                 Ataque Cuatro Peones
+B06               Defensa Moderna
+B07-B09                 Defensa Pirc
+      B08                 Variante Clasica
+      B09                 Ataque Austriaco
+B10-B19                 Defensa Caro-Kann
+      B11                 Defensa Dos Caballos
+      B12                 Variante del Avance
+      B14                 Ataque Panov
+B20-B99                 Defensa Siciliana
+      B21                 Gambito Morra; Ataque Grand Prix
+      B22                 Variante Alapin
+      B23-B26                   Variante Cerrada
+      B29                 Variante Rubinstein
+      B30-B31                   Variante Rossolimo
+      B32                 Variante Lowenthal
+      B33                 Variante Sveshnikov
+      B34-B39                   Variante Dragon Acelerado
+            B37-B38                      Maroczy Bind
+      B40                 Variante del Contrataque
+      B41-B49                   Variante Paulsen
+            B41-B43                      Sistema Kan
+            B44                   Sistema Taimanov
+            B45                   Sistema Clasico
+            B46-B47                      Sistema Taimanov
+      B50                 Variante Kopec
+      B51-B52                   Variante Rossolimo
+      B53                 Variante Hungara
+      B55                 Variante Anti-Dragon
+      B57                 Variante Sozin-Benko
+      B58                 Variante Boleslavsky
+      B60-B69                   Ataque Richter Rauzer
+      B70-B79                   Variante Dragon
+            B72                   Sistema Clasico
+            B76-B78                      Ataque Yugoslavo
+      B80-B89                   Variante Scheveningen
+            B81                   Sistema Keres
+            B86-B87                      Sistema Fischer/Sozin
+      B90-B99                   Sistema Najdorf
+            B96                   Sistema Goteborg
+            B97                   Sistema del Peon Envenenado
+C00-C20                 Defensa Francesa
+      C01                 Variante del Cambio
+      C02                 Variante del Avance
+      C03-C09                   Variante Tarrasch
+            C03                   Sistema 3.... a6
+            C04                   Sistema 3.... Cc6
+            C05-C06                      Sistema 3.... Cf6
+            C07-C09                      Sistema 3.... c5
+      C10                 Variante Rubinstein
+      C11                 Variante Steinitz
+      C12                  Variante MacCutcheon
+      C15-C20                    Variante Winawer
+           C15                     Sistema Lapus Manus
+           C17                     Sistema Suizo
+           C18                     Linea Principal
+           C20                     Sistema del Cambio
+C21               Gambito Nordico
+C22               Partida de Centro
+C23-C24                 Apertura del Alfil
+C25-C29                 Apertura Vienesa
+C30-C39                 Gambito de Rey
+      C31-C32                   Contragambito Falkbeer
+      C33                 Variante Keres
+      C35                 Variante Cunningham
+      C36                 Variante Moderna
+      C37                 Gambito Muzio
+      C38                 Gambitos Greco y Philidor
+      C39                 Gambitos Allagier y Kiesertisky
+C40               Gambito Leton
+C41               Defensa Philidor
+C42-C43                 Defensa Rusa (Petroff)
+C44               Apertura Ponziani
+C45               Apertura Escocesa
+C46               Apertura Tres Caballos
+C47-C49                 Apertura Cuatro Caballos
+C50-C59                 Apertura Italiana (Guioco Piano)
+      C50                 Defensa Hungara
+      C51-C52                   Gambito Evans
+      C55                 Defensa Dos Caballos
+      C56                 Ataque Max Lange
+      C57-C59                   Defensa Dos Caballos
+C60-C99                 Apertura Española (Ruy Lopez)
+      C61                 Defensa Bird
+      C62                 Defensa Steinitz Antigua
+      C63                 Gambito Schliemann
+      C64                 Defensa Cordel
+      C65                 Defensa Berlin
+      C66                 Defensa Steinitz Moderna
+      C67                 Defensa Rio de Janeiro
+      C68-C69                   Variante del Cambio
+      C71-C76                   Defensa Steinitz Moderna
+      C77                 Variante Anderssen
+      C78                 Variante Arcangel; Ataque Moeller
+      C79                 Variante Rusa
+      C80-C83                   Variante Abierta
+            C80                    Sistema Bernstein
+            C81                    Sistema Keres
+            C83                    Linea Principal
+      C84                 Ataque de Centro
+      C85                 Variante del Cambio Doblemente Diferida
+      C86                 Ataque Worral
+      C88                 Gambito Marshall
+      C92                 Variante Zaitsev
+      C93                 Variante Smyslov
+      C94-C95                   Variante Breyer
+      C96-C99                   Tchigorin
+D00               Apertura de Peon de Dama
+D01               Apertura Veresov
+D02               Apertura Gruenfeld Invertida
+D03               Ataque Torre, Variante Tartakower
+D04               Apertura de Peon de Dama
+D05               Sistema Colle
+D06-D69                 Gambito de Dama
+      D07                 Variante Tchigorin
+      D08-D09                   Contragambito Albin
+      D10-D19                   Defensa Eslava
+            D14                   Sistema del Cambio
+            D15                   Gambito Geller
+            D17                   Sistema Checo
+            D18                   Linea Principal
+            D19                   Sistema Euwe
+      D20-D29                   Aceptado
+            D21                   Sistema Borsenko-Furman
+      D30-D69                   Rehusado
+            D31                   Sistema Alatortsev
+            D32-D33                      Sistema Tarrasch
+            D34                   Sistema Rubinstein
+            D35-D36                      Sistema del Cambio
+            D37                   Sistema 5.Af4
+            D38-D39                      Defensa Ragozin
+            D40-D42                      Defensa Semi-Tarrasch
+            D43                   Defensa Semi-Eslava
+            D44-D45                      Defensa Anti-Merano
+            D46                   Defensa Semi-Eslava
+            D47-D49                      Defensa Merano
+            D51-D52                      Defensa Cambridge Springs
+            D53-D54                      Defensa Ortodoxa
+            D55                   Sistema Anti-Tartakower
+            D56                   Sistema Lasker
+            D57                   Sistema Moderna
+            D58-D59                      Sistema Tartakower
+            D60-D62                      Defensa Ortodoxa
+            D63-D66                      Sistema Blackburne
+            D67                   Sistema Capablanca
+            D68                   Sistema Blackburne
+D70-D99                 Defensa Grunfeld
+      D71                 Variante Neo-Grunfeld
+      D76                 Variante Ortodoxa del Fianchetto
+      D79                 Variante Eslava
+      D82-D84                   Sistema Af4
+      D85                 Variante del Cambio Moderna
+      D87                 Variante del Cambio Botvinnik
+      D89                 Variante del Cambio
+      D93                 Sistema Ag3
+"""
+
+
+lines = eco_lines.strip().split('\n')
+
+
+eco_codes = []
+openings = []
+variations = []
+
+
+for line in lines:
+    # Separar el código ECO del nombre de la apertura o variante
+    match = re.match(r"(\w\d{2}(?:-\w\d{2})?)\s+(.*)", line.strip())
+    if match:
+        eco_code = match.group(1)
+        description = match.group(2)
+        
+        if "-" in eco_code:  # Rango de códigos ECO
+            start_code, end_code = eco_code.split("-")
+            start_letter = start_code[0]
+            start_number = int(start_code[1:])
+            end_number = int(end_code[1:])
+            
+            for i in range(start_number, end_number + 1):
+                eco_codes.append(f"{start_letter}{str(i).zfill(2)}")
+                if "Variante" in description or "Sistema" in description:
+                    openings.append("")
+                    variations.append(description.strip())
+                else:
+                    openings.append(description.strip())
+                    variations.append("")
+        else:
+            eco_codes.append(eco_code)
+            if "Variante" in description or "Sistema" in description:
+                openings.append("")
+                variations.append(description.strip())
+            else:
+                openings.append(description.strip())
+                variations.append("")
+
+
+eco_df = pd.DataFrame({
+    "ECO": eco_codes,
+    "Apertura": openings,
+    "Variación": variations
+})
+
+pgn_file_path = r'I:\ITBA\Stockfish_16_64-bit_4CPU.commented.[1986].pgn'
+
+
+pgn = open(pgn_file_path)
+
+
+blancas = []
+negras = []
+resultado_blancas = []
+resultado_negras = []
+eco_codes = []
+
+
+while True:
+    game = chess.pgn.read_game(pgn)
+    if game is None:
+        break
+
+
+    blancas.append(game.headers["White"])
+    negras.append(game.headers["Black"])
+    
+    resultado = game.headers["Result"]
+    if resultado == "1-0":
+        resultado_blancas.append(1.0)
+        resultado_negras.append(0.0)
+    elif resultado == "0-1":
+        resultado_blancas.append(0.0)
+        resultado_negras.append(1.0)
+    else:
+        resultado_blancas.append(0.5)
+        resultado_negras.append(0.5)
+    
+    eco_codes.append(game.headers["ECO"])
+
+
+df_partidas = pd.DataFrame({
+    "Blancas": blancas,
+    "Negras": negras,
+    "Resultado Blancas": resultado_blancas,
+    "Resultado Negras": resultado_negras,
+    "ECO": eco_codes
+})
+
+
+df_merged = pd.merge(df_partidas, eco_df, on="ECO", how="left")
+
+# Exportar el DataFrame resultante a un archivo Excel
+df_merged.to_excel("I:/ITBA/partidas_con_aperturas_completas.xlsx", index=False)
+
+print("El archivo Excel 'partidas_con_aperturas_completas.xlsx' se ha creado exitosamente.")
